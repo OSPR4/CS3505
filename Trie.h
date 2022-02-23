bdab14aef67ca339fcb2971d5ed441aca7450dd1
OSPR4 <u1351688@utah.edu>
2022-02-22 20:19:44 -0700

    Formatting

--- Trie.h
+++ Trie.h
@@ -15,22 +15,14 @@ class Trie
 {
     private:
         Node root;
-
         void traverseTrie(Node current, std::string word,  std::vector<std::string> &wordList);
-
         Node searchTrie(std::string word);
 
     public:
         Trie();
-
-
-
         void addAWord(std::string word);
-
         bool isAWord(std::string word);
-       
         std::vector<std::string> allWordsStartingWithPrefix(std::string word);
 
-    
 };
 #endif
